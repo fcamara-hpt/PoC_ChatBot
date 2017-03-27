cfb563e124a9438b47fe283151c2bae0fba19bb8
Rafael Lopes Silva <rafael.lopes@fcamara.com.br>
2017-03-27 14:43:04 -0500

    Update 1

--- app.js
+++ app.js
@@ -118,25 +118,25 @@ function sendMessage(sender, text_) {
 			    }]
 			    }
 		    }
-	    }
+	    };
 	
 		
 		request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData2,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-	}
+	        url: 'https://graph.facebook.com/v2.6/me/messages',
+	        qs: { access_token: token },
+	        method: 'POST',
+	        json: {
+	            recipient: { id: sender },
+	            message: messageData2,
+	        }
+	    }, function (error, response, body) {
+	        if (error) {
+	            console.log('Error sending message: ', error);
+	        } else if (response.body.error) {
+	            console.log('Error: ', response.body.error);
+	        }
+	    });
+		}
 	
 	
 	
