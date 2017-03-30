3e55fce9b4d6459671ca9557da1c8e0fb16dba63
Rafael Lopes Silva <rafael.lopes@fcamara.com.br>
2017-03-30 15:37:21 -0300

    Commit

--- app.js
+++ app.js
@@ -112,7 +112,7 @@
 
 
 		console.log("Text_: " + text_ + " + Tamanho de text_: " + text_.length + " + Sender: " + sender);
-
+		now = new Date;
 		console.log("getDay: " + now.getDay() + ", getData: " + now.getData() + ", getMonth: " + now.getMonth());
 
 
