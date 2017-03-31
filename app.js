5aff7e90d52fd92a4a1e253fc6f4c8f987347caf
Rafael Lopes Silva <rafael.lopes@fcamara.com.br>
2017-03-31 14:49:09 -0300

    Commit

--- app.js
+++ app.js
@@ -116,6 +116,17 @@
 
 	function sendMessageData(sender, data){
 		var hoje = new Date();
+		var dd = hoje.getDate();
+		var mm = hoje.getMonth()+1;
+		var yyyy = hoje.getFullYear();
+
+		if(dd<10){
+		    dd='0'+dd;
+		} 
+		if(mm<10){
+		    mm='0'+mm;
+		}
+		var hoje = yyyy + '-' + mm + '-' + dd;
 
 		console.log("Hoje: " + hoje + ", Data: " + data);
 
